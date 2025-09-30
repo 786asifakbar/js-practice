7dbb7109387be70bee618c179f73563b22d8c0f9
Asif Akbar <149171617+786asifakbar@users.noreply.github.com>
2025-09-30 20:14:38 +0000

    typeconversion

---
+++ basics/conversionDatatypes.js
@@ -0,0 +1,26 @@
+let userAge = true; // boolean
+let userAgeNum = Number(userAge); // converting boolean to number
+let userAgeStr = String(userAgeNum); // converting number to string
+let userAgeBool = Boolean(userAgeStr); // converting string to boolean
+let userAgeNum2 = Number(userAgeBool); // converting boolean to number
+let userAgeStr2 = String(userAgeNum2); // converting number to string
+let userAgeBool2 = Boolean(userAgeStr2); // converting string to boolean
+let userAgeNum3 = Number(userAgeBool2); // converting boolean to number
+let userAgeStr3 = String(userAgeNum3); // converting number to string
+let userAgeBool3 = Boolean(userAgeStr3); // converting string to boolean
+let userAgeNum4 = Number(userAgeBool3); // converting boolean to number
+let userAgeStr4 = String(userAgeNum4); // converting number to string
+let userAgeBool4 = Boolean(userAgeStr4); // converting string to boolean
+let userAgeNum5 = Number(userAgeBool4); // converting boolean to number
+let userAgeStr5 = String(userAgeNum5); // converting number to string
+let userAgeBool5 = Boolean(userAgeStr5); // converting string to boolean
+let userAgeNum6 = Number(userAgeBool5); // converting boolean to number
+let userAgeStr6 = String(userAgeNum6); // converting number to string
+let userAgeBool6 = Boolean(userAgeStr6); // converting string to boolean
+let userAgeNum7 = Number(userAgeBool6); // converting boolean to number
+
+let str = String(userAgeNum); // converting number to string
+
+
+console.log(str);
+console.log(typeof str);
