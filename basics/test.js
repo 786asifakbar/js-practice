5a3afc771dc8ed7ca144bc35512b505eabcb972f
Asif Akbar <149171617+786asifakbar@users.noreply.github.com>
2025-09-30 17:31:30 +0000

    container setup

--- basics/test.js
+++ basics/test.js
@@ -2,3 +2,7 @@ console.log("Wellcome back");
 console.log("This is frist console");
 console.log("This is second console");
 console.log("This is third console");
+console.log("This is fourth console");
+console.log("This is fifth console");
+console.log("This is sixth console");
+console.log("This is seventh console");
