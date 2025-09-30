512c7121b2179422dba6533a8d1b3bab4f0c0f5b
Asif Akbar <149171617+786asifakbar@users.noreply.github.com>
2025-09-30 19:48:37 +0000

    variables and datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,26 @@
+let userAge = 30; // number
+const piValue = 3.14; // number
+var isMember = false; // boolean
+let userAddress = null; // object
+let undefinedVar; // undefined
+let sym = Symbol("unique"); // symbol
+let bigIntNum = 9007199254740991n; // bigint
+let str = "Hello, World!"; // string
+
+
+
+console.log("String:", str);
+console.log("Number:", userAge);
+
+console.log("Pi Value:", piValue);
+console.log("BigInt Number:", bigIntNum);
+
+
+
+
+console.log("Symbol:", sym);
+console.log("User Address:", userAddress);
+console.log("Undefined Variable:", undefinedVar);
+console.log("User Age:", userAge);
+console.log("Pi Value:", piValue);
+console.log("Is Member:", isMember);
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345; // integer
+let userName = "JohnDoe"; // string
+var isLoggedIn = true; // boolean
+
+console.log("Account ID:", accountId);
+console.log("User Name:", userName);
+console.log("Is Logged In:", isLoggedIn);
+
+// Reassigning variable
+userName = "JaneDoe";
+console.log("Updated User Name:", userName); 
