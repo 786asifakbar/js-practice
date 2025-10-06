80087c1f6dea1fa70b353cd34eea6f1d20b77e2f
Asif Akbar <149171617+786asifakbar@users.noreply.github.com>
2025-10-06 11:15:01 +0000

    add arrow function

---
+++ basics/ArrowFunction.js
@@ -0,0 +1,56 @@
+//let user = {
+
+//username : "Hitesh",
+//price : 999,
+
+//welcomMassage : function(){
+//console.log(`${this.username} , wellcome to website`);
+//console.log(this);
+//}
+ 
+//}
+// user.welcomMassage();
+ //user.username = "Asif"
+ //user.welcomMassage();
+ 
+ 
+//function chai(){
+ //const username = "Hitesh";
+//console.log(this.username);
+
+//}
+
+//chai(this.username) 
+//console.log(this.username);
+  
+  
+  
+  /*
+  let chai = () => {
+  const username = "Hitesh";
+  console.log(`${this.username} , wellcome back`);
+  
+  };
+   chai();
+  */
+  
+  //const addtwo = (no1,no2) => {    // explicite return me curly braces {} or return key word lagana hota hai
+  //return no1 + no2;
+  
+  //}
+  
+//  console.log(addtwo(2,4));
+  
+  //implicite return me perun thesis lagana parta hai or curly braces {} or return keyword nh lagana parta hai
+  //const addnum =(n1 , n2)=> n1 + n2;
+  
+  const addnum =(n1 , n2)=> (n1 + n2);
+  console.log(addnum(2,3));
+  
+  
+  //way to use arrow function in loop
+  //  const array = [];
+  // array.forEach(() => ());
+  // array.forEach(() => {});
+  // array.forEach(() => );
+  
