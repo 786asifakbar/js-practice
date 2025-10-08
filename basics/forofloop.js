482f065536516f0b0548424a3120e018bae37ed0
Asif Akbar <149171617+786asifakbar@users.noreply.github.com>
2025-10-08 18:41:48 +0000

    updated

---
+++ basics/forofloop.js
@@ -0,0 +1,89 @@
+ for await (const element of object) {
+    console.log(element);
+}
+for (const element of array) {
+    console.log(element);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+    
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}
+for (let char of "Hello") {
+    console.log(char);
+}   
+for (let char of "Hello") {
--- basics/loops.js
+++ basics/loops.js
@@ -24,7 +24,7 @@
     }
         console.log("i: " + i);                         
     }       
-    }                               32`  log("i: " + i);
+    }    32`  log("i: " + i);
         }
         console.log("i: " + i);
     }
