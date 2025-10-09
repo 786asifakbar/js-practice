4d364b8760aed699a5b2fe2a11bf40b115e17382
Asif Akbar <149171617+786asifakbar@users.noreply.github.com>
2025-10-09 06:06:27 +0000

    forOf Update

--- basics/forofloop.js
+++ basics/forofloop.js
@@ -16,3 +16,73 @@ for (const element of array) {
 for (let char of "Hello") {
     console.log(char);
 };
+
+// for of loop used in array ,string, and other things
+  
+            let arr = [ 
+            1,2,3,4,5,6,7 
+            ]; 
+			for(const ar of arr){ 
+			//console.log(`this is for of loop ${ar}`); 
+			}; 
+			
+            const fruits = ["Apple","Banana","mangose","pineApple"]; 
+            for(const fruite of fruits){ 
+		    //console.log(`Fruite : ${fruite}`); 
+	        }		    
+			
+			for(fruite in fruits){ 
+    		//	console.log(`For In loop : ${fruite}`); 
+			} 
+			
+			let x = "Asif Akbars"; 
+			//console.log(`Before for of loop : ${x}`); 
+			for(const y of x){ 
+			//console.log(`After for of loop : ${y}`); 
+			} 
+			
+			let a = "31523455";  
+            for(const b of a){ 
+			console.log(b); 
+			} 
+  
+            let a = 315342343;  
+            for(const b of a){ 
+			console.log(b); 
+			} 
+  
+  
+			let a = "true";  
+            for(const b of a){ 
+			console.log(b); 
+			} 
+  			
+			
+			let a = true;   
+            for(const b of a){ 
+			console.log(b); 
+			} 
+			
+			
+			let numbers = new Set([1,2,3,4,5,6,7,8]) 
+			for(const num of numbers){ 
+			console.log(`sets : ${num}`); 
+			} 
+			
+			
+			let map = new Map([
+			["name", "Asif",],
+			["role","Developer"]
+			]);
+			for(const [key,value] of map){
+			console.log(key,"=",value);
+			}
+			
+			
+						}
+			
+			
+			
+			
+			
+			
