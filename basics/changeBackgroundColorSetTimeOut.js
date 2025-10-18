5adbae105a084e125eb4e2a2587c680583b35f66
Asif Akbar <149171617+786asifakbar@users.noreply.github.com>
2025-10-18 17:21:10 +0000

    setTioutAgain

---
+++ basics/changeBackgroundColorSetTimeOut.js
@@ -0,0 +1,164 @@
+document.body.style.transition = "background-color 1s";
+setTimeout(() => {
+  document.body.style.backgroundColor = "lightblue";
+}, 2000);
+setTimeout(() => {
+  document.body.style.backgroundColor = "lightgreen";
+}, 4000);
+setTimeout(() => {
+  document.body.style.backgroundColor = "lightcoral";
+}, 6000);
+setTimeout(() => {
+  document.body.style.backgroundColor = "lightgoldenrodyellow";
+}, 8000);
+setTimeout(() => {
+  document.body.style.backgroundColor = "white";
+}, 10000);  
+console.log("Background color will change every 2 seconds for 10 seconds.");
+window.addEventListener("load", (event) => {
+  console.log("Page fully loaded:", event);
+});
+window.addEventListener("beforeunload", (event) => {
+  console.log("Page is about to be unloaded:", event);
+});
+window.addEventListener("unload", (event) => {
+  console.log("Page unloaded:", event);
+});
+window.addEventListener("focus", (event) => {
+  console.log("Window focused:", event);
+});         
+window.addEventListener("blur", (event) => {
+    console.log("Window lost focus:", event);
+    }   
+
+);
+window.addEventListener("visibilitychange", (event) => {
+    console.log("Visibility changed. Document is now:", document.visibilityState);
+}); 
+window.addEventListener("scroll", (event) => {
+    console.log("Window scrolled:", event);
+}); 
+window.addEventListener("resize", (event) => {
+    console.log("Window resized:", event);
+});
+window.addEventListener("keydown", (event) => {
+    console.log("Key pressed:", event);
+});
+window.addEventListener("keyup", (event) => {
+    console.log("Key released:", event);
+}); 
+window.addEventListener("mousemove", (event) => {
+    console.log("Mouse moved:", event);
+}); 
+window.addEventListener("click", (event) => {
+     console.log("Window clicked:", event);
+}); 
+window.addEventListener("dblclick", (event) => {
+
+    console.log("Window double-clicked:", event);
+}); 
+window.addEventListener("contextmenu", (event) => {
+    console.log("Context menu opened:", event);
+});             
+window.addEventListener("load", (event) => {
+    console.log("Page fully loaded:", event);
+}); 
+window.addEventListener("beforeunload", (event) => {
+    console.log("Page is about to be unloaded:", event);
+}); 
+window.addEventListener("unload", (event) => {
+    console.log("Page unloaded:", event);
+});
+window.addEventListener("scroll", (event) => {
+    console.log("Window scrolled:", event);
+}); 
+window.addEventListener("resize", (event) => {
+    console.log("Window resized:", event);
+}); 
+window.addEventListener("keydown", (event) => {
+    console.log("Key pressed:", event);
+}); 
+window.addEventListener("keyup", (event) => {
+    console.log("Key released:", event);
+}); 
+window.addEventListener("mousemove", (event) => {
+    console.log("Mouse moved:", event);
+}); 
+window.addEventListener("click", (event) => {
+    console.log("Window clicked:", event);
+}); 
+window.addEventListener("dblclick", (event) => {
+    console.log("Window double-clicked:", event);
+}); 
+window.addEventListener("contextmenu", (event) => {
+    console.log("Context menu opened:", event);
+}); 
+window.addEventListener("focus", (event) => {
+    console.log("Window focused:", event);
+}); 
+window.addEventListener("blur", (event) => {
+    console.log("Window lost focus:", event);
+}); 
+window.addEventListener("visibilitychange", (event) => {
+    console.log("Visibility changed. Document is now:", document.visibilityState);
+}); 
+window.addEventListener("load", (event) => {
+    console.log("Page fully loaded:", event);
+}); 
+window.addEventListener("beforeunload", (event) => {
+    console.log("Page is about to be unloaded:", event);
+}); 
+window.addEventListener("unload", (event) => {
+    console.log("Page unloaded:", event);
+});
+window.addEventListener("scroll", (event) => {
+    console.log("Window scrolled:", event);
+}); 
+window.addEventListener("resize", (event) => {
+    console.log("Window resized:", event);
+}); 
+window.addEventListener("keydown", (event) => {
+    console.log("Key pressed:", event);
+}); 
+window.addEventListener("keyup", (event) => {
+    console.log("Key released:", event);
+}); 
+window.addEventListener("mousemove", (event) => {
+    console.log("Mouse moved:", event);
+}); 
+window.addEventListener("click", (event) => {
+    console.log("Window clicked:", event);
+}); 
+window.addEventListener("dblclick", (event) => {
+    console.log("Window double-clicked:", event);
+}); 
+window.addEventListener("contextmenu", (event) => {
+    console.log("Context menu opened:", event);
+}); 
+window.addEventListener("focus", (event) => {
+    console.log("Window focused:", event);
+}); 
+window.addEventListener("blur", (event) => {
+    console.log("Window lost focus:", event);
+}); 
+window.addEventListener("visibilitychange", (event) => {
+    console.log("Visibility changed. Document is now:", document.visibilityState);
+}); 
+window.addEventListener("load", (event) => {
+    console.log("Page fully loaded:", event);
+}); 
+window.addEventListener("beforeunload", (event) => {
+    console.log("Page is about to be unloaded:", event);
+}); 
+window.addEventListener("unload", (event) => {
+    console.log("Page unloaded:", event);
+});
+window.addEventListener("scroll", (event) => {
+    console.log("Window scrolled:", event);
+}); 
+window.addEventListener("resize", (event) => {
+    console.log("Window resized:", event);
+}); 
+window.addEventListener("keydown", (event) => {
+    console.log("Key pressed:", event);
+});             
