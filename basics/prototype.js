2a106d9a53aaca741bea68d8cfe747abbc63d9a6
Asif Akbar <149171617+786asifakbar@users.noreply.github.com>
2025-10-23 15:30:29 +0000

    prototype

---
+++ basics/prototype.js
@@ -0,0 +1,22 @@
+let myHeros =["thor" , "spiderman"];
+
+
+let Heros ={
+       thor :  "hummer",
+       spiderman : "sling",
+
+    getSpiderPower : function(){
+      console.log(`welcome spiderman ${this.spiderman}`);
+     Object.prototype.asif = function(){
+
+        console.log(`POWER OF ASIF`);
+      } 
+    
+    }
+         
+
+    }
+
+    Heros.asif();
+      
+
